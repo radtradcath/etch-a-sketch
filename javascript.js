9afb11e947b4e8788e4f9d823ed736f3144bb4a5
radtradcath <vinicius.b.bonafini@gmail.com>
2023-07-25 15:44:42 -0300

    Create event handler button to change grid size
    -index.html features a new button for the user to click;
    
    -style.css now features flexbox changes to set fixed grid size, so
    the content has a max-width and max-height to adjust itself when
    user changes the grid;
    
    -javascript.js now features the button's event handler. Several changes
    wore made to make the script change accordingly to user user's grid
    preference. There's no more need to refresh browser page and change
    createContainers() parameters to set a new grid. Button now handles it.

--- javascript.js
+++ javascript.js
@@ -1,33 +1,53 @@
 const gridContainer = document.querySelector('#grid-container');
+const btnChangeGridSize = document.querySelector('#change-size');
+
 let box = document.createElement('div');
 let container = document.createElement('div');
-let numBox = 16;
-let numContainer = 16;
+let numBox = 20;
+let numContainer = 20;
+let userSize;
 
-function createContainers () {
-    for (i = 0; i < numContainer ; i++) {
+createContainers(numContainer, numBox);
+
+
+
+function createContainers(numContainer, numBox) {
+    for (i = 0; i < numContainer; i++) {
         container = document.createElement('div');
         container.classList.add('box-container');
         gridContainer.appendChild(container);
 
         for (j = 0; j < numBox; j++) {              //Creates 16 containers with 16 boxes each;
 
-        box = document.createElement('div');
-        box.classList.add('box-child');
-        container.appendChild(box);
+            box = document.createElement('div');
+            box.classList.add('box-child');
+            container.appendChild(box);
         }
 
     }
+    const boxChilds = document.querySelectorAll('.box-child');
+    boxChilds.forEach((box) => box.addEventListener('mouseenter', () => { //For each box of nodelist, change background color when hovered;
+        box.setAttribute('style', 'background-color: red;');
+    }));
 }
-createContainers();     //Call the function to generate containers and boxes;
 
-const boxChilds = document.querySelectorAll('.box-child');
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild)
+    }
+}
 
-boxChilds.forEach((box) => box.addEventListener('mouseenter', () => { //For each box of nodelist, change background color when hovered;
-    box.setAttribute('style', 'background-color: red;');
-}));
 
+btnChangeGridSize.addEventListener('click', () => {
+    userSize = Number(prompt('What is the size of the box?'));
+    console.log(typeof userSize);
+    if (typeof userSize === 'number' && userSize < 100) {
+        removeAllChildNodes(container);
+        removeAllChildNodes(gridContainer);
+        createContainers(userSize, userSize);
+        
+    } else {
+    return alert('Input a valid number of less than 100.');
+    }
+});
 
-//addEventListener to each box 'mouseneter';
-//pass a function to change the background color of the box when hovered;
-//Make so that the background color stays;
