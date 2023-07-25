03ccc6e44087f5b0eac8c90ec37e848c890c0748
radtradcath <vinicius.b.bonafini@gmail.com>
2023-07-25 11:44:08 -0300

    Add 'mouseenter' event to change boxes bg color

--- javascript.js
+++ javascript.js
@@ -4,15 +4,13 @@ let container = document.createElement('div');
 let numBox = 16;
 let numContainer = 16;
 
-//Create 16 containers with 16 boxes each;
-
 function createContainers () {
     for (i = 0; i < numContainer ; i++) {
         container = document.createElement('div');
         container.classList.add('box-container');
         gridContainer.appendChild(container);
 
-        for (j = 0; j < numBox; j++) {
+        for (j = 0; j < numBox; j++) {              //Creates 16 containers with 16 boxes each;
 
         box = document.createElement('div');
         box.classList.add('box-child');
@@ -21,5 +19,15 @@ function createContainers () {
 
     }
 }
+createContainers();     //Call the function to generate containers and boxes;
 
-createContainers();
+const boxChilds = document.querySelectorAll('.box-child');
+
+boxChilds.forEach((box) => box.addEventListener('mouseenter', () => { //For each box of nodelist, change background color when hovered;
+    box.setAttribute('style', 'background-color: red;');
+}));
+
+
+//addEventListener to each box 'mouseneter';
+//pass a function to change the background color of the box when hovered;
+//Make so that the background color stays;
